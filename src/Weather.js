f251e9b3698400ef6a0808b39f52576d1a6ffba8
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-11-14 13:22:37 +0000

    removed target

--- src/Weather.js
+++ src/Weather.js
@@ -38,9 +38,7 @@ export default function Weather() {
       <div className="row weather-forecast">
         <div className="col-2">
           <h3> 19:00 </h3>
-		<a href="https://github.com/elicodes89/weather-app" target="_blank">Open-source code </a> by EliCodes
-
-
+		<a href="https://github.com/elicodes89/weather-app"> Open-source code </a> by EliCodes
         </div>
       </div>
     </div>
