9e65a936ce8d9350c4972df2d8b5f4300aa19f1e
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-11-21 16:17:14 +0000

    started to build HTML React App

--- src/Weather.js
+++ src/Weather.js
@@ -4,45 +4,15 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <div className="Weather">
-        <div className="City">
-          <form className="search-form">
-            <input
-              type="text"
-              placeholder="Enter a city"
-              autocomplete="off"
-              autofocus="on"
-            />
-            <input type="submit" value="🔍" />
-          </form>
-        </div>
-      </div>
+   <div className="container">
+	   <h1>Dalkeith</h1>
+	   <ul>
+		   <li>Saturday 03:00</li>
+		   <li>It is Raining</li>
+	   </ul>
+   <div className="row">
+	   <div className="col-6">
 
-      <div className="appcontent">
-        <h5></h5>
-        <h1>Dalkeith</h1>
-      </div>
-
-      <div className="alt">Rain</div>
-      <div className="notes">
-        Currently:
-        <h4>Test</h4>
-        <span className="temperature"></span>
-        <span className="units">
-          <a href="/">℃</a>
-          <a href="/">°F</a>
-        </span>
-        <span className="wDescription">Cloudiness: 10%</span> 
-        <span className="humidity">Humidity: 10%</span>  
-        <span className="wind-speed">Wind: 4km/h</span> 
-      </div>
-      <div className="row weather-forecast">
-        <div className="col-2">
-          <h3> 19:00 </h3>
-		<a href="https://github.com/elicodes89/weather-app"> Open-source code </a> by EliCodes
-        </div>
-      </div>
-    </div>
-  );
-}
+   </div>
+   </div>
+   </div>
