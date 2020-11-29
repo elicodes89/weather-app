62e1d3211230c78ce6e5005b2f2f2066c9362088
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-11-29 15:52:11 +0000

    fixed url

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,20 +1,21 @@
 import React from "react";
 import TheDate from "./TheDate";
+import WeatherIcon from "./WeatherIcon";
 
 
 export default function WeatherData(props){
 	return (
 		<div className="WeatherData">
 	<h1>{props.data.city}</h1>
-	   {/* <div className="image">
+	   <div className="image">
+		   <WeatherIcon code={props.data.icon}
 		   <img
 		   src={props.data.iconUrl}
 		   alt={props.data.description}
 		   className="weatherIcon"
 		   />		   
-	   </div> */}
-	   		   <div className="fecha">{props.data.date}</div>
-
+	   </div>
+<div className="fecha">{props.data.date.getHours()}</div>	
 	   <div className= "text-capitalize">{props.data.description}
 		   <div className="temperature">{Math.round(props.data.temperature)}</div>
 		   Precipitation: <br></br>
