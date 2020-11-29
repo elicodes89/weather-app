8e056dbb5bb585461e7733bb0b5ffeaf8f1df097
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-11-29 17:47:37 +0000

    created weathertemp component to change from celsius to farenheit

--- src/WeatherData.js
+++ src/WeatherData.js
@@ -1,22 +1,26 @@
 import React from "react";
 import TheDate from "./TheDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemp from "./WeatherTemp";
 
 
 export default function WeatherData(props){
 	return (
 		<div className="WeatherData">
 	<h1>{props.data.city}</h1>
-	<WeatherTemp celsius={props.data.temperature}  />
-	   <div className="float-left">
-		   <WeatherIcon code={props.data.icon} />
-		   		   
+			<div className="fecha"> <TheDate date={props.data.date} /></div>
+
+	
+	   <div className="temp">
+		   		<WeatherTemp celsius={props.data.temperature} />
+		<WeatherIcon code={props.data.icon} />		   		   
 	   </div>
-		<div className="fecha"> <TheDate date={props.data.date} /></div>
-		<div className= "text-capitalize">{props.data.description}
+		<div className="details">
+		<div className= "text-capitalize">{props.data.description}<br></br>
 		   Precipitation: <br></br>
 		   Humidity: {props.data.humidity} %<br></br>
 		   Wind: {props.data.wind} km/h <br></br>
+		   </div>
 	</div>
    </div>
 	);
